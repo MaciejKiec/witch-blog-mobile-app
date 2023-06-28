1026e2942689b5ca88382bb8af9574553136f7fe
MaciejKiec <harmonicisland8@gmail.com>
2023-06-28 22:18:12 +0200

    feat: Added MainActity screen

---
+++ app/src/main/java/com/kpz/redesign/MainActivity.java
@@ -0,0 +1,37 @@
+package com.kpz.redesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // remove title
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home_page);
+    }
+
+    public void TakePictureActivity(View view) {
+    }
+
+    public void OpenBrowser(View view) {
+        Intent openBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://witch.azurewebsites.net/"));
+        startActivity(openBrowser);
+    }
+}
