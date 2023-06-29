362deafa9cf6d96eb02985f836823f2abd650f66
MaciejKiec <harmonicisland8@gmail.com>
2023-06-29 13:33:00 +0200

    feat: Added OpenCV dependency

--- app/src/main/java/com/kpz/redesign/DetectedCards.java
+++ app/src/main/java/com/kpz/redesign/DetectedCards.java
@@ -3,6 +3,9 @@ package com.kpz.redesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class DetectedCards extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class DetectedCards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detected_cards);
+
+        if(OpenCVLoader.initDebug()) Log.d("Opencv","loaded!");
     }
 }
